01abb23173e6a9e160fdf974edd566a304fbf02e
DEEPAK SURANA <deepaksurana18@gmail.com>
2017-01-13 15:43:54 -0500

    changing database to mlab
    direct remote db connection

--- test/app.js
+++ test/app.js
@@ -4,7 +4,7 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    var connectionString = 'mongodb://ds163758.mlab.com:63758/web-dev-db/test';
 
     if(process.env.MLAB_USERNAME) {
         connectionString = process.env.MLAB_USERNAME + ":" +
